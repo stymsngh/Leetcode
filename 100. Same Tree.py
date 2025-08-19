65864a189c190aa424151c9cdbf2609bf7cde3c4
Satyam Singh <iamstymsingh@gmail.com>
2025-08-19 23:03:51 +0530

    100. Same Tree

---
+++ 100. Same Tree.py
@@ -0,0 +1,26 @@
+"""
+Question: 100. Same Tree
+Status: Done
+Difficulty: Easy
+Author: iamss
+Created: 19/08/25
+"""
+
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+
+        if p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right):
+            return True
+        return False
