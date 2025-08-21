734a82c3910652ba9a626a7fdceb600c048b449a
Satyam Singh <iamstymsingh@gmail.com>
2025-08-21 22:37:24 +0530

    70. Climbing Stairs

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,22 @@
+"""
+Question: 70. Climbing Stairs
+Status: Done
+Difficulty: Easy
+Author: iamss
+Created: 21/08/25
+"""
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n - 1]
